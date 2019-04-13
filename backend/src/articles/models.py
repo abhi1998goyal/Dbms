7ad2e199ca5b65f045a6817f42653fabd275f9a8
abhi1998goyal <abhi1998goyal@gmail.com>
2019-04-13 11:08:48 -0400

    Update models.py

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -14,8 +14,8 @@ class Organizer(Member):
     # organizer_name=model.CharField(max_length=30)
     society=models.CharField(max_length=20,choices=societies)
     ident_no=models.IntegerField(primary_key=True)
-    def __int__(self):
-        return self.ident_no
+    def __str__(self):
+        return str(self.ident_no)
 class Events(models.Model):
     events_type=(('s','Sports'),('t','Technical'),('c','Cultural'))
     events_div=(('w','workshop'),('com','competition'),('con','conference'))
@@ -24,19 +24,19 @@ class Events(models.Model):
     event_div=models.CharField(max_length=20,choices=events_div)
     event_name=models.CharField(max_length=20)
     organizer_id=models.OneToOneField(Organizer,on_delete=models.CASCADE,primary_key=True)
-    poster_img=models.ImageField(upload_to=None)
+    poster_img=models.ImageField()
     def __str__(self):
         return self.event_name
 class Student(Member):
     roll_no=models.IntegerField(primary_key=True)
     event_part=models.ManyToManyField(Events,through='Registration')
     def __str__(self):
-        return self.roll_no
+        return str(self.roll_no)
 class Registration(models.Model):
     part_id=models.ForeignKey(Student,on_delete=models.CASCADE)
     event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
     def __str__(self):
-        return self.part_id + " "+self.event_id
+        return str(self.part_id) + " "+str(self.event_id)
 
 
     
