57001cc163ec17684291ae59ff379dd864b21eb5
abhi1998goyal <abhi1998goyal@gmail.com>
2019-04-20 14:50:27 -0400

    Update models.py

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -1,23 +1,20 @@
 from django.db import models
-# class Member(models.Model):
-#     name =models.CharField(max_length=120)
-#     email_id=models.EmailField(default='')
-#     phone_no=models.IntegerField(default=9999999999)
+from django.core.mail import send_mail
+class Member(models.Model):
+    name =models.CharField(max_length=120)
+    email_id=models.EmailField()
+    phone_no=models.IntegerField()
 
-#     class Meta:
-#         abstract=True
+    class Meta:
+        abstract=True
 
-#     def __str__(self):
-#         return self.name
-class Organizer(models.Model):
+    def __str__(self):
+        return self.name
+class Organizer(Member):
     societies=(('TNT','thapr nautanki team'),('scim','Scimatics'),('IETE','Indian Institute of technical Eng'))
     # organizer_name=model.CharField(max_length=30)
-    name =models.CharField(max_length=120)
-    email_id=models.CharField(max_length=50)
-    phone_no=models.IntegerField(null=False,default=9999999999)
     society=models.CharField(max_length=20,choices=societies)
     ident_no=models.IntegerField(primary_key=True)
-    
     def __str__(self):
         return str(self.ident_no)
 class Events(models.Model):
@@ -28,24 +25,32 @@ class Events(models.Model):
     event_div=models.CharField(max_length=20,choices=events_div)
     event_name=models.CharField(max_length=20)
     organizer_id=models.OneToOneField(Organizer,on_delete=models.CASCADE,primary_key=True)
-    #organizer_id=models.ForeignKey(Organizer,on_delete=models.CASCADE)
     poster_img=models.ImageField()
     def __str__(self):
         return self.event_name
-class Student(models.Model):
-    name =models.CharField(max_length=120)
-    email_id=models.CharField(max_length=50)
-    phone_no=models.IntegerField(null=False,default=9999999999)
+class Student(Member):
     roll_no=models.IntegerField(primary_key=True)
     event_part=models.ManyToManyField(Events,through='Registration')
-    
-    def __int__(self):
-        return self.roll_no
+    def __str__(self):
+        return str(self.roll_no)
 class Registration(models.Model):
     part_id=models.ForeignKey(Student,on_delete=models.CASCADE)
     event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
+    class Meta:
+        unique_together=('part_id','event_id')
     def __str__(self):
         return str(self.part_id) + " "+str(self.event_id)
+#     def save(self):
+#         number=self.objects.all().count()
+#         send_mail(
+#      'Subject here',
+#         'Here is the message.',
+#      'abhi1998goyal@gmail.com',
+#      ['agoyal5_be16@thapar.edu'],
+#      fail_silently=False,
+# ) 
+#         super.save(self)
+
 
 
     
