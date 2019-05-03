144ef96a7d3343cf2fc003e577f107e7c7395018
abhi1998goyal <abhi1998goyal@gmail.com>
2019-05-02 21:56:36 -0400

    Update views.py
    email added

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from articles.models import Organizer,Events,Student,Registration
 from .serializers import OrganizerSerializer,EventsSerializer,StudentSerializer,RegistrationSerializer
+from django.core.mail import send_mail
+from djreact import settings
+from django.http import HttpResponse
+
 # class ArticleListView(ListAPIView):
 #     queryset=Article.objects.all()
 #     serializer_class=ArticleSerializer
@@ -23,3 +27,13 @@ class StudentViewSet(viewsets.ModelViewSet):
 class RegistrationViewSet(viewsets.ModelViewSet):
     queryset=Registration.objects.all()
     serializer_class=RegistrationSerializer
+#     @action(detail=True)
+    
+def subscribe(request,email):
+    send_mail(
+     'Subject here',
+        'Here is the message.',
+     settings.DEFAULT_FROM_EMAIL,
+     ['agoyal5_be16@thapar.edu'],
+     fail_silently=False,)
+    return HttpResponse("<h1>Thanks for Registering<h1>")
